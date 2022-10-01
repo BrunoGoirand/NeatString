3496eec0b35398f6fa06a8bc1a066f06d9fe30e4
Bruno Goirand <geebe@goirand.com>
2022-10-01 08:08:01 +0200

    Fixing '.gitignore' file

--- NeatString/include/NeatString.h
+++ NeatString/include/NeatString.h
@@ -5,10 +5,6 @@
  * 
  */
 
-char *ltrim(char *); // left trim
-char *rtrim(char *); // right trim
-char *trim(char *);  // trim (left+right)
-
 /**
  * 
  */
@@ -17,18 +13,19 @@ int charHap(char *, char);
 int charSpaceHap(char *);
 int stringHap(char *, char *);
 
-
-
 /**
  * WARNING - functions allocating memory with 'malloc()' - pointer must be freed
  */
 char *lowerCase(char *);
 char *upperCase(char *);
 
+char *ltrim(char *); // left trim
+char *rtrim(char *); // right trim
+char *trim(char *);  // trim (left+right)
+
 char *itrim(char *); // inside trim (left+right+inside) - WARNING 
+
 char *removeCPPComments(char *); // remove C++ comments '//'
-
-
 
 
 /**
